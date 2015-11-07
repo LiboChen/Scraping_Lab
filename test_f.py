6250e340368b12499dc4d13a691874ae415ea2fc
Libo Chen <chenlibo0928@gmail.com>
2015-11-06 19:38:17 -0600

    add unit test

--- test_f.py
+++ test_f.py
@@ -1,9 +1,28 @@
-import json
-import yahoo_options_data
+#!/usr/bin/env python
+"""Unit tests for the project.yahoo_options_data."""
+from unittest import TestCase
+from mock import patch
 
-computedJson = yahoo_options_data.contractAsJson("f.dat")
+
+import json
+import project.yahoo_options_data
+
+computedJson = project.yahoo_options_data.contractAsJson("f.dat")
 expectedJson = open("f.json").read()
 expectedJson_change = open("f_change.json").read()
+
+
+class StandAloneTests(TestCase):
+    """Test the stand-alone module functions."""
+    @patch('__builtin__.open')
+    def test_aapl(self, mock_open):
+        """Test the yahoo_option_data function."""
+        mock_open.return_value.read.return_value = \
+            "f.dat\n"
+        self.assertTrue(
+		json.loads(computedJson) == json.loads(expectedJson) or json.loads(computedJson) == json.loads(expectedJson_change)
+		)
+
 
 if json.loads(computedJson) != json.loads(expectedJson) and json.loads(computedJson) != json.loads(expectedJson_change):
   print "Test failed!"
